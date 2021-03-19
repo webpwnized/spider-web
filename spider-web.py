3d528fe6d3419fcd8d455e608562d3a6b8e9d472
webpwnized <webpwnized@gmail.com>
2021-03-19 15:28:13 -0400

    1.0.42 Added documentation for new report

--- spider-web.py
+++ spider-web.py
@@ -8,7 +8,7 @@ import config as __config
 from argparse import RawTextHelpFormatter
 import argparse
 
-l_version = '1.0.41'
+l_version = '1.0.42'
 
 def print_version() -> None:
     if Parser.verbose:
