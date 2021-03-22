20b8ca8e8e61018212b268cb495ad41250331e1f
webpwnized <webpwnized@gmail.com>
2021-03-22 14:54:10 -0400

    1.0.42 Issues Report: Made sure scan type is full

--- scans.py
+++ scans.py
@@ -16,7 +16,7 @@ class Scans():
 
      def append_if_better(self, p_candidate_scan: dict) -> None:
          try:
-             if p_candidate_scan["IsCompleted"] and p_candidate_scan["State"] == "Complete":
+             if p_candidate_scan["IsCompleted"] and p_candidate_scan["State"] == "Complete" and p_candidate_scan["ScanType"] == "Full":
                  l_scan_profile_id: str = p_candidate_scan["ScanTaskProfileId"]
                  if l_scan_profile_id in self.__m_scans:
                      l_new_scan_create_datetime: datetime = parser.parse(p_candidate_scan["InitiatedAt"])
