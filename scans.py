7d0595f82b31f7f720398359e50b68efe1f6a7b8
Justin <justinfrank@ups.com>
2022-08-26 13:41:49 -0400

    Updating BSC code

--- scans.py
+++ scans.py
@@ -14,17 +14,28 @@ class Scans():
      def count(self) -> int:
          return len(self.__m_scans)
 
+     def is_excluded(self, p_candidate_scan: dict) -> bool:
+        l_scan_tags = p_candidate_scan["Tags"] 
+        l_profile_tags = p_candidate_scan.get("ScanTaskProfile", {}).get("Tags")
+        l_tags = l_scan_tags + l_profile_tags
+        if ("Not for BSC" in l_tags):
+            l_scan_id = p_candidate_scan["ScanTaskProfileId"]
+            self.__mPrinter.print(f"Excluded Scan Id: {l_scan_id} \t Tags: {l_tags}", Level.INFO)
+            return True
+        return False
+
      def is_complete(self, p_candidate_scan: dict) -> bool:
         if (p_candidate_scan["IsCompleted"]
                 and p_candidate_scan["State"] == "Complete"
-                and p_candidate_scan["ScanType"] == "Full"
+                and (p_candidate_scan["ScanType"] == "Full" 
+                    or p_candidate_scan["ScanType"] == "Incremental")
                 and p_candidate_scan["ScanTaskProfileId"]):
             return True
         return False
 
      def append_if_better(self, p_candidate_scan: dict) -> None:
          try:
-             if self.is_complete(p_candidate_scan):
+             if self.is_complete(p_candidate_scan) and not self.is_excluded(p_candidate_scan):
                  l_scan_profile_id: str = p_candidate_scan["ScanTaskProfileId"]
                  if l_scan_profile_id in self.__m_scans:
                      l_new_scan_create_datetime: datetime = parser.parse(p_candidate_scan["InitiatedAt"])
